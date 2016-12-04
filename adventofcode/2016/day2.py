cfb5a7b7862fa349596c9249d2e53b5be34a538d
Andreas Karlsson <andreas@snowflakeinteractive.com>
2016-12-04 20:37:50 +0100

    day 2 part 2

--- adventofcode/2016/day2.py
+++ adventofcode/2016/day2.py
@@ -10,7 +10,7 @@ with open('input2.txt', 'r') as f:
 
     for line in f:
 
-        print('Starting from {}'.format((pos_x + 1, pos_y + 1)))
+        # print('Starting from {}'.format((pos_x + 1, pos_y + 1)))
 
         for c in line.strip(' '):
             if c == 'U':
@@ -23,8 +23,70 @@ with open('input2.txt', 'r') as f:
                 pos_x = pos_x + 1 if pos_x + 1 <= 1 else 1
 
         # Move the coordinates
-        print(pos_x+1, pos_y+1)
         sequence.append(buttons[pos_y+1][pos_x+1])
 
-    print(''.join(sequence))
+    print('Part 1: ' + ''.join(sequence))
 
+# Part 2
+with open('input2.txt', 'r') as f:
+
+    def can_i_go(direction, x, y):
+        if direction == 'U':
+            if x == 2 and y > 0:
+                return True
+            elif (x == 1 or x == 3) and y > 1:
+                return True
+            else:
+                return False
+        elif direction == 'D':
+            if x == 2 and y < 4:
+                return True
+            elif (x == 1 or x == 3) and y < 3:
+                return True
+            else:
+                return False
+        elif direction == 'L':
+            if y == 2 and x > 0:
+                return True
+            elif (y == 1 or y == 3) and x > 1:
+                return True
+            else:
+                return False
+        elif direction == 'R':
+            if y == 2 and x < 4:
+                return True
+            elif (y == 1 or y == 3) and x < 3:
+                return True
+            else:
+                return False
+        return False
+
+    buttons = [['', '', '1', '', ''],
+               ['', '2', '3', '4', ''],
+               ['5', '6', '7', '8', '9'],
+               ['', 'A', 'B', 'C', ''],
+               ['', '', 'D', '', ''],
+               ]
+    sequence = []
+
+    pos_x = 0
+    pos_y = 2
+
+    for line in f:
+
+        # print('Starting from {}'.format((pos_x + 1, pos_y + 1)))
+
+        for c in line.strip(' '):
+            if can_i_go(c, pos_x, pos_y):
+                if c == 'U':
+                    pos_y -= 1
+                elif c == 'D':
+                    pos_y += 1
+                elif c == 'L':
+                    pos_x -= 1
+                elif c == 'R':
+                    pos_x += 1
+
+        sequence.append(buttons[pos_y][pos_x])
+
+    print('Part 2: ' + ''.join(sequence))
