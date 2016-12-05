f0fcd1ce0a1ed8947827bf6af0feb51f3a2fb0f9
Andreas Karlsson <andreas@snowflakeinteractive.com>
2016-12-05 21:57:51 +0100

    day 4 part 2

--- adventofcode/2016/day4.py
+++ adventofcode/2016/day4.py
@@ -18,5 +18,24 @@ with open('input4.txt', 'r') as f:
                                              key=lambda t: (-t[1], t[0]))][:5])
         if word == checksum:
             total += number
+            word = line.strip().rsplit('-', 1)[0]
+            sector_id = int(line.strip().rsplit('-', 1)[1].split('[')[0])
+            decrypted = ''
+            char_range = ord('z') - ord('a') + 1
+            positions_to_shift = sector_id % char_range
+
+            for c in word:
+                if c == '-':
+                    decrypted += ' '
+                elif ord('a') <= ord(c) <= ord('z'):
+                    if not ord(c) + positions_to_shift > ord('z'):
+                        decrypted += chr(ord(c) + positions_to_shift)
+                    else:
+                        decrypted += chr(
+                            ord(c) + positions_to_shift - char_range)
+
+            if decrypted == 'northpole object storage':
+                north_pole_sector = sector_id
 
     print('Part 1: {}'.format(total))
+    print('Part 2: {}'.format(north_pole_sector))
