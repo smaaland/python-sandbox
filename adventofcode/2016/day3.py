c6de320eb8b3b9b24fe7fec539bd7dfdc53a45e7
Andreas Karlsson <andreas@snowflakeinteractive.com>
2016-12-05 20:13:03 +0100

    day 3 part 2

--- adventofcode/2016/day3.py
+++ adventofcode/2016/day3.py
@@ -10,3 +10,22 @@ with open('input3.txt', 'r') as f:
             possible += 1
 
     print('Part 1: {}'.format(possible))
+
+with open('input3.txt', 'r') as f:
+    possible = 0
+
+    data = []
+
+    for line in f:
+
+        data.append([int(i) for i in re.split('\s+', line.strip())])
+
+        if len(data) == 3:
+            for x in range(3):
+                parts = sorted([data[y][x] for y in range(3)])
+                if parts[0] + parts[1] > parts[2]:
+                    possible += 1
+
+            data = []
+
+    print('Part 2: {}'.format(possible))
