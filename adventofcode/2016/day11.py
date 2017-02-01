fc9de55d3883f5a4d03a5568609f88df073e12d3
smaaland <andreas.karlsson@britny.se>
2017-02-01 15:19:13 +0100

    more work on day 11

--- adventofcode/2016/day11.py
+++ adventofcode/2016/day11.py
@@ -15,12 +15,13 @@
 
 # if a chip (0) is ever left in the same area as another RTG, and it's not
 # connected to its own RTG, the chip will be fried.
+from copy import deepcopy
 
 
 class State:
     def __init__(self, level, pairs):
         self.level = level
-        self.pairs = sorted(pairs)
+        self.pairs = sorted(deepcopy(pairs))
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
@@ -66,44 +67,57 @@ def validate_state(state, old_states):
     return True
 
 
-def get_possible_moves(state, old_states):
-    l = state.level
+def get_possible_moves(current_state, old_states):
+    old_states.append(current_state)
 
-    if l == 1:
+    if current_state.level == 1:
         possible_levels = [2]
-    elif l == 4:
+    elif current_state.level == 4:
         possible_levels = [3]
     else:
-        possible_levels = [l - 1, l + 1]
+        possible_levels = [current_state.level - 1, current_state.level + 1]
 
     possibilities = []
     for level in possible_levels:
+        print('level: {}'.format(level))
         # select ONE
+        for x in range(2):
+            for y in range(len(current_state.pairs)):
+                if current_state.pairs[y][x] == current_state.level and abs(
+                        level - current_state.level) == 1:
+                    new_state = State(level=level, pairs=current_state.pairs)
+                    new_state.pairs[y][x] += (level - current_state.level)
+                    # print('adding {} to {} {}'.format(
+                    #     level - current_state.level, y, x))
+                    # print(new_state)
+                    possibilities.append(new_state)
 
         # select TWO
 
         # We can bring AT MOST two items (no matter which) in ONE direction
         # select one or two of any type to bring THAT ARE ON THIS LEVEL
 
-        for p in state.pairs:
-            print(p)
-            # TODO return the states that's possible
+        # for p in current_state.pairs:
+        #     print(p)
+        #     # TODO return the states that's possible
+        #
+        #     new_pairs = []
+        #     new_state = State(level=level, pairs=new_pairs)
+        #     if validate_state(new_state, old_states=old_states):
+        #         possibilities.append(new_state)
 
-            new_pairs = []
-            new_state = State(level=level, pairs=new_pairs)
-            if validate_state(new_state, old_states=old_states):
-                possibilities.append(new_state)
-
+    for p in possibilities:
+        print(p)
     return possibilities
 
 
 floor = 1
 instructions = []
-current_state = False
+# current_state = False
 states = [parse_initial_state()]
 
 print(states)
 
 # keep states in the recursive algorithm or loop somehow
-possible_moves = get_possible_moves(states[-1], states)
+possible_moves = get_possible_moves(states[-1], [])
 print(possible_moves)
