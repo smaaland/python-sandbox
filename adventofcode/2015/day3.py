53b270b34af71e2b38753173cd4a3c6addac24fd
smaaland <andreas.karlsson@billogram.com>
2017-10-19 13:07:26 +0200

    day 3 part 2

--- adventofcode/2015/day3.py
+++ adventofcode/2015/day3.py
@@ -5,7 +5,25 @@ with open('input3.txt', 'r') as f:
         for c in line:
             last = coordinates[-1]
             if c == '<':
-                coordinates.append((last[0]-1, last[1]))
+                coordinates.append((last[0] - 1, last[1]))
+            elif c == '>':
+                coordinates.append((last[0] + 1, last[1]))
+            elif c == '^':
+                coordinates.append((last[0], last[1] + 1))
+            elif c == 'v':
+                coordinates.append((last[0], last[1] - 1))
+
+    print(len(set(coordinates)))
+
+with open('input3.txt', 'r') as f:
+    total = 0
+    coordinates = [(0, 0), (0, 0)]
+    for line in f:
+        for c in line:
+
+            last = coordinates[-2]
+            if c == '<':
+                coordinates.append((last[0] - 1, last[1]))
             elif c == '>':
                 coordinates.append((last[0] + 1, last[1]))
             elif c == '^':
