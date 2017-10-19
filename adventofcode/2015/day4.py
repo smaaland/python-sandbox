8f3b4e3289e4dac3dc9d8345cd8e180412625db9
smaaland <andreas.karlsson@billogram.com>
2017-10-19 13:15:48 +0200

    day 4

---
+++ adventofcode/2015/day4.py
@@ -0,0 +1,10 @@
+import hashlib
+
+input_str = 'bgvyzdsv'
+
+num = 0
+while True:
+    if hashlib.md5((input_str + str(num)).encode('utf-8')).hexdigest().startswith('000000'):
+        print(num)
+        break
+    num += 1
