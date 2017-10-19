d6e1f165f6f2e612b785dcde894314a7780371d9
smaaland <andreas.karlsson@billogram.com>
2017-10-19 11:39:56 +0200

    day 2 part 1

--- adventofcode/2015/day2.py
+++ adventofcode/2015/day2.py
@@ -1,9 +1,11 @@
 with open('input2.txt', 'r') as f:
     total = 0
     for line in f:
-        parts = line.split('x')
-        smallest = 100000
-        for exclude in range(3):
-            to_check = [x for x in range(3) if x != exclude]
-            total += to_check[0] * to_check[1]
-            #smallest = total if total < smallest
+        sides = [int(i) for i in line.split('x')]
+        # normal sides
+        total += 2 * sides[0] * sides[1] + 2 * sides[1] * sides[2] + \
+            2 * sides[0] * sides[2]
+        # area of the smallest side
+        total += min([sides[0] * sides[1], sides[1] * sides[2],
+                     sides[0] * sides[2]])
+print(total)
