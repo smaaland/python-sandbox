57d44f416066bbf7491cfec57d1f09dba8f10ea0
smaaland <andreas.karlsson@billogram.com>
2018-01-05 13:54:24 +0100

    day 13

--- adventofcode/2015/day13.py
+++ adventofcode/2015/day13.py
@@ -1,9 +1,28 @@
 struct = {}
 
-with open('input13.txt', 'r') as f:
 
+def calculate_score(current):
+    score = 0
+
+    for x in range(-1, len(current) - 1):
+        score += struct[current[x]][current[x + 1]]
+        score += struct[current[x + 1]][current[x]]
+
+    return score
+
+
+def iterate(current, left, scores):
+    if len(left):
+        for p in left:
+            iterate(current + [p], [x for x in left if x != p], scores)
+    else:
+        # Calculate score
+        scores.append(calculate_score(current))
+    return scores
+
+
+with open('input13.txt', 'r') as f:
     for line in f:
-        # print(line)
         parts = line.split(' ')
         p1 = parts[0]
         p2 = parts[-1].split('.')[0]
@@ -12,38 +31,19 @@ with open('input13.txt', 'r') as f:
             struct[p1] = {}
         struct[p1][p2] = val
 
-struct['smaaland'] = {}
+for p1 in struct:
+    scores1 = iterate([p1], [x for x in struct if x != p1], [])
 
+print(max(scores1))
+
+
+struct['smaaland'] = {}
 for x in struct:
     struct['smaaland'][x] = 0
     struct[x]['smaaland'] = 0
 
-# print(struct)
-scores = []
-
-
-def calculateScore(current):
-    score = 0
-
-    for x in range(-1, len(current)-1):
-        score += struct[current[x]][current[x+1]]
-        score += struct[current[x+1]][current[x]]
-
-    return score
-
-def iterate(current, left):
-    # print(current)
-    # print(left)
-    if len(left):
-        for p in left:
-            # TODO append won't work
-            iterate(current + [p], [x for x in left if x != p])
-    else:
-        # Calculate score
-        # print(current)
-        scores.append(calculateScore(current))
 
 for p1 in struct:
-    iterate([p1], [x for x in struct if x != p1])
+    scores2 = iterate([p1], [x for x in struct if x != p1], [])
 
-print(max(scores))
+print(max(scores2))
