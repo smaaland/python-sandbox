7ba5b65464ed7595eeebe2fa6b22d85724956390
smaaland <andreas.karlsson@billogram.com>
2018-01-05 12:58:51 +0100

    day 8

--- adventofcode/2015/day8.py
+++ adventofcode/2015/day8.py
@@ -1,38 +1,9 @@
-import re
-d2 = 0
-cumul_raw = 0
-cumul_view = 0
-
+total = 0
+total_2 = 0
 with open('input8.txt', 'r') as f:
-
     for line in f:
-        line=line.rstrip()
-        d2+=4
+        total += len(line[:-1]) - len(eval(line))
+        total_2 += (2 + line.count('\\') + line.count('"'))
 
-        cumul_raw+=len(line)
-        # print(line)
-
-        for i in range(1,len(line)):
-            # print(d2)
-            if line[i] == "\\" and not (i == len(line)-2):
-                if line[i+1] == "\"":
-                    d2+=2
-                elif line[i+1] == '\\' and (not line[i-1] == '\\'):
-                    d2+=2
-                elif line[i+1] == "x":
-                    if re.findall(r'[a-f0-9]{2}',line[i+2:i+4]):
-                        d2+=1
-print(d2)
-print(cumul_raw)
-print(cumul_raw - d2)
-print(cumul_raw + d2)
-
-
-with open("input8.txt") as f:
-    strings = f.readlines()
-
-count = 0
-for x in strings:
-    temp = x.strip()
-    count += 2 + temp.count('"') + temp.count('\\')
-print(count)
+print(total)
+print(total_2)
