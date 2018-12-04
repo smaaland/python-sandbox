b117f4d7647ed1fb53bef6d3a60968cbe88e9f47
smaaland <smaaland86@gmail.com>
2018-12-04 21:38:25 +0100

    day 1 2018

---
+++ adventofcode/2018/day1.py
@@ -0,0 +1,31 @@
+lines = []
+with open('input1.txt', 'r') as f:
+    for line in f:
+        line = line.strip()
+        lines.append(line)
+
+    sum = 0
+    for line in lines:
+        operator = line[0]
+        val = int(line[1:])
+        if operator == '+':
+            sum += val
+        elif operator == '-':
+            sum -= val
+    print(f'Part 1: {sum}')
+
+    sum = 0
+    visited = set()
+    while True:
+        for line in lines:
+            operator = line[0]
+            val = int(line[1:])
+            if operator == '+':
+                sum += val
+            elif operator == '-':
+                sum -= val
+            if sum in visited:
+                print(f'Part 2: {sum}')
+                exit()
+            visited.add(sum)
+
