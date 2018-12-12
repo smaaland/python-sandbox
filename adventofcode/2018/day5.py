3bf27efc73574c1515cb8a9703849d1f451a5f72
smaaland <smaaland86@gmail.com>
2018-12-13 00:03:51 +0100

    day 5 part 1

---
+++ adventofcode/2018/day5.py
@@ -0,0 +1,43 @@
+import string
+
+
+def react(s):
+    removed_something = True
+    start = 0
+    while removed_something:
+        removed_something = False
+        index = start
+        for i, j in zip(s[start::1], s[start + 1::1]):
+            if i.lower() == j.lower() and ((i.isupper() and j.islower()) or (i.islower() and j.isupper())):
+                removed_something = True
+                s = s[0:index] + s[index + 2:]
+                start = index - 1
+                if start < 0:
+                    start = 0
+                break
+            index += 1
+            removed_something = False
+    return s
+
+
+data = ''
+with open('input5.txt', 'r') as f:
+    for line in f:
+        data += line.strip()
+
+    # input = 'dabAcCaCBAcCcaDA'
+
+    print(f'Part 1: {len(react(data))}')
+    # print(len(data))
+
+    lengths = []
+    for char in string.ascii_lowercase:
+        print(char)
+        s = data.strip(char).strip(char.upper())
+        print(len(s))
+        c = react(s)
+        print(len(c))
+        lengths.append(len(c))
+
+    print(lengths)
+    print(f'Part 2: {min(lengths)}')
