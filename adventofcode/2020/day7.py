3864b5dc702a3e5df230cea0199350b9186a2929
smaaland <smaaland86@gmail.com>
2020-12-18 01:17:53 +0100

    day 7 part 2

--- adventofcode/2020/day7.py
+++ adventofcode/2020/day7.py
@@ -13,16 +13,32 @@ def get_all_parents(_parents, child, so_far):
     return so_far
 
 
+def get_all_children(_children, child):
+    if child in _children:
+        return sum([c['amount'] + c['amount'] * get_all_children(_children, c['type']) for c in _children[child]])
+    else:
+        return 0
+
+
 parents = {}
+children = {}
+
 for line in lines:
     bag, contents = line.strip('.').split(' bags contain ')
     for content in contents.split(', '):
         if content == 'no other bags':
             continue
-        _bag = ' '.join(content.split(' ')[1:-1])
+        parts = content.split(' ')
+        _bag = ' '.join(parts[1:-1])
+        amount = int(parts[0])
         if _bag not in parents:
             parents[_bag] = []
         if bag not in parents[_bag]:
             parents[_bag].append(bag)
+        if bag not in children:
+            children[bag] = []
+        if _bag not in children[bag]:
+            children[bag].append({'type': _bag, 'amount': amount})
 
 print(f"Part 1: {len(list(set(get_all_parents(parents, my_bag, []))))}")
+print(f"Part 2: {get_all_children(children, my_bag)}")
