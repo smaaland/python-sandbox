53ee438b67c6fae9d14f9e3047228df1fb301288
smaaland <smaaland86@gmail.com>
2021-12-07 23:40:59 +0100

    day 3

--- adventofcode/2021/day3.py
+++ adventofcode/2021/day3.py
@@ -1,19 +1,30 @@
+from collections import Counter
+
 with open('input3.txt', 'r') as f:
     lines = [line.rstrip() for line in f]
 
-ones = [0] * 12
-zeros = [0] * 12
-
-for line in lines:
-    for i, d in enumerate(line):
-        if int(d) == 0:
-            zeros[i] += 1
-        if int(d) == 1:
-            ones[i] += 1
 gamma = ''
 epsilon = ''
-for i in range(12):
-    gamma += '1' if ones[i] > zeros[i] else '0'
-    epsilon += '1' if ones[i] < zeros[i] else '0'
+for i in range(len(lines[0])):
+    count = Counter([line[i] for line in lines])
+    gamma += max(count, key=count.get)
+    epsilon += min(count, key=count.get)
 
 print(f"Part 1: {int(gamma, 2) * int(epsilon, 2)}")
+
+
+lines_oxygen = lines
+for i in range(len(lines_oxygen[0])):
+    count = Counter([line[i] for line in lines_oxygen])
+    lines_oxygen = [_ for _ in lines_oxygen if _[i] == max(count, key=count.get)]
+    if len(lines_oxygen) == 1:
+        break
+
+lines_co2 = lines
+for i in range(len(lines_co2[0])):
+    count = Counter([line[i] for line in lines_co2])
+    lines_co2 = [_ for _ in lines_co2 if _[i] == min(count, key=count.get)]
+    if len(lines_co2) == 1:
+        break
+
+print(f"Part 2: {int(lines_oxygen[0], 2) * int(lines_co2[0], 2)}")
