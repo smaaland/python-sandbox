05dce27eae3bf03340c01a58166a37c12970cf80
smaaland <andreas.karlsson@billogram.com>
2021-12-10 14:30:09 +0100

    day 5

--- adventofcode/2021/day5.py
+++ adventofcode/2021/day5.py
@@ -1,50 +1,54 @@
+from typing import Dict, List
+
 with open('input5.txt', 'r') as f:
     lines = [line.rstrip() for line in f]
 
-data = [_.split(" -> ") for _ in lines]
-max_x = 0
-max_y = 0
-
-lines = []
-for d in data:
-    lines.append({
+rows = [
+    {
         "x1": int(d[0].split(",")[0]),
         "y1": int(d[0].split(",")[1]),
         "x2": int(d[1].split(",")[0]),
         "y2": int(d[1].split(",")[1]),
-    })
-    print(lines)
-    print(type(max_x))
-    print(type(lines["x1"]))
-    if lines["x1"] > max_x:
-        max_x = lines["x1"]
-    if lines["x2"] > max_x:
-        max_x = lines["x2"]
-    if lines["y1"] > max_y:
-        max_y = lines["y1"]
-    if lines["y2"] > max_y:
-        max_y = lines["y2"]
+    } for d in [_.split(" -> ") for _ in lines]
+]
 
-print(max_x, max_y)
+max_x = max(max([_["x1"] for _ in rows]), max([_["x2"] for _ in rows]))
+max_y = max(max([_["y1"] for _ in rows]), max([_["y2"] for _ in rows]))
+matrix_1 = [[0 for _ in range(max_x + 1)] for __ in range((max_y + 1))]
+matrix_2 = [[0 for _ in range(max_x + 1)] for __ in range((max_y + 1))]
 
-for l in lines:
-    matrix = [max_x * [max_x * 0]]
 
-lines_to_use = []
-for l in lines:
+def get_all_points_on_line(l: Dict) -> List[List[int]]:
+    min_x = min(l["x1"], l["x2"])
+    max_x = max(l["x1"], l["x2"])
+    min_y = min(l["y1"], l["y2"])
+    max_y = max(l["y1"], l["y2"])
+    reversed_x = l["x1"] > l["x2"]
+    reversed_y = l["y1"] > l["y2"]
     if l["x1"] == l["x2"]:
-        lines_to_use.append(l)
+        return [[min_x, y] for y in range(min_y, max_y+1)]
     if l["y1"] == l["y2"]:
-        lines_to_use.append(l)
+        return [[x, min_y] for x in range(min_x, max_x+1)]
 
-for l in lines_to_use:
-    if not l["x1"] == l["x2"]:
-        for x in range(l["x1"], l["x2"]+1):
-            print(type(l["y1"]))
-            print(l["y1"])
-            print(type(x))
-            print(x)
-            matrix[l["y1"][x]] = "X"
-    if not l["y2"] == l["y2"]:
-        for y in range(l["y1"], l["y2"]+1):
-            print(y)
+    x_values = [_ for _ in range(min_x, max_x + 1)]
+    if reversed_x:
+        x_values = reversed(x_values)
+    y_values = [_ for _ in range(min_y, max_y + 1)]
+    if reversed_y:
+        y_values = reversed(y_values)
+    return list(map(list, zip(x_values, y_values)))
+
+
+for row in rows:
+    points = get_all_points_on_line(row)
+    for x, y in points:
+        if row["x1"] == row["x2"] or row["y1"] == row["y2"]:
+            matrix_1[y][x] += 1
+        matrix_2[y][x] += 1
+
+
+part_1 = sum([1 if _ > 1 else 0 for __ in matrix_1 for _ in __])
+part_2 = sum([1 if _ > 1 else 0 for __ in matrix_2 for _ in __])
+print(f"Part 1: {part_1}")
+print(f"Part 2: {part_2}")
+
