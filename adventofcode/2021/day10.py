02549ae983056ae0add470eb203c372444cabae1
smaaland <smaaland86@gmail.com>
2021-12-15 22:18:01 +0100

    day 10 part 2

--- adventofcode/2021/day10.py
+++ adventofcode/2021/day10.py
@@ -1,3 +1,5 @@
+import math
+
 with open('input10.txt', 'r') as f:
     lines = [line.rstrip() for line in f]
 
@@ -14,18 +16,33 @@ points = {
     '}': 1197,
     '>': 25137,
 }
+completion_points = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4,
+}
 
 errors = []
+completion_scores = []
+
 for line in lines:
     stack = []
+    error = False
     for c in line:
         if c in matching_chars.values():
             stack.append(c)
         else:
             if stack.pop() != matching_chars[c]:
                 errors.append(c)
+                error = True
                 continue
+    if not error:
+        score = 0
+        for c in reversed(stack):
+            score = score * 5 + completion_points[c]
+        completion_scores.append(score)
 
 print(f"Part 1: {sum([points[k] for k in errors])}")
-
+print(f"Part 2: {sorted(completion_scores)[math.floor(len(completion_scores)/2)]}")
 
