ed8abcab71eaaf3ba0b634fa54aa6200669730cf
smaaland <smaaland86@gmail.com>
2022-12-09 21:40:57 +0100

    day 7 part 2

--- adventofcode/2022/day7.py
+++ adventofcode/2022/day7.py
@@ -1,41 +1,38 @@
 from collections import defaultdict
 
-lines = [line.strip("\n") for line in open('input7.txt', 'r').readlines()]
+lines = [line.strip("\n") for line in open("input7.txt", "r").readlines()]
 
-terminal_output = []
 path = []
 sizes = defaultdict(int)
 part_1_total = 0
 max_size = 100000
-total_space = 70000000
-update_size = 30000000
-used_space = sizes['/']
-free_space = total_space - used_space
-space_needed = update_size - free_space
 
 
 for line in lines:
-    if line.startswith('$ cd'):
+    if line.startswith("$ cd"):
         directory = line.split()[-1]
-        if directory == '..':
+        if directory == "..":
             path.pop()
         else:
             path.append(directory)
-    elif line.startswith('$ ls'):
+    elif line.startswith("$ ls"):
         continue
     else:
         size, _ = line.split()
         if size.isdigit():
             size = int(size)
             for i in range(len(path)):
-                sizes['/'.join(path[:i + 1])] += size
+                sizes["/".join(path[:i + 1])] += size
 
 options = []
 for key, value in sizes.items():
     if value <= max_size:
         part_1_total += value
-    if value > space_needed:
-        options.append(value)
+    options.append(value)
+
+used_space = sizes["/"]
+free_space = 70000000 - used_space
+space_needed = 30000000 - free_space
 
 print(f"Part 1: {part_1_total}")
-print(f"Part 2: {min(options)}")
+print(f"Part 2: {min([o for o in options if o > space_needed])}")
