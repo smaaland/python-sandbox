789a9fb04d1dde7b2c84b197aaaf8dbc00eba16d
smaaland <andreas.karlsson@billogram.com>
2022-12-12 16:58:08 +0100

    day 8 part 2 started

--- adventofcode/2022/day8.py
+++ adventofcode/2022/day8.py
@@ -25,4 +25,29 @@ for x in range(len(lines[0])):
             visible.add((y,x))
             highest = int(lines[y][x])
 
+for y in range(len(lines)):
+    for x in range(len(lines[y])):
+        if x == 0 or y == 0 or x == len(lines[y]) - 1 or y == len(lines[x]) - 1:
+            continue
+        height = int(lines[y][x])
+
+        left = 0
+        for xl in reversed(range(x)):
+            if int(lines[y][xl]) < height:
+                left += 1
+            else:
+                left += 1
+                break
+        # print(y, x, left)
+        right = 0
+        for xl in range(x+1, len(lines[y])):
+            # print(xl)
+            if int(lines[y][xl]) < height:
+                right += 1
+            else:
+                right += 1
+                break
+        # print(y, x, right)
+
+
 print(f"Part 1: {len(visible)}")
