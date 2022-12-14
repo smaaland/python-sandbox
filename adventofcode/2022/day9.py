6c390ed0f4963dae4474cb8cbc8f8aaa40aea7e7
smaaland <smaaland86@gmail.com>
2022-12-14 23:32:01 +0100

    day 9 part 2

--- adventofcode/2022/day9.py
+++ adventofcode/2022/day9.py
@@ -1,16 +1,16 @@
 lines = [line.strip().split() for line in open("input9.txt", "r").readlines()]
-print(lines)
-hx, hy, tx, ty = 0, 0, 0, 0
+knots = [[0, 0] for _ in range(10)]
 tail_visits = set()
-tail_visits.add((tx, ty))
+tail_visits_2 = set()
+tail_visits.add(tuple(knots[1]))
+tail_visits_2.add(tuple(knots[-1]))
 
 
-def touching():
+def touching(hx, hy, tx, ty):
     return abs(hx-tx) <= 1 and abs(hy-ty) <= 1
 
 
-def move_tail():
-    global hx, hy, tx, ty
+def move_tail(hx, hy, tx, ty):
     if hx > tx:
         tx += 1
     elif hx < tx:
@@ -19,20 +19,26 @@ def move_tail():
         ty += 1
     elif hy < ty:
         ty -= 1
+    return [tx, ty]
 
 
 for l in lines:
     for _ in range(int(l[1])):
         if l[0] == "R":
-            hx += 1
+            knots[0][0] += 1
         if l[0] == "L":
-            hx -= 1
+            knots[0][0] -= 1
         if l[0] == "U":
-            hy += 1
+            knots[0][1] += 1
         if l[0] == "D":
-            hy -= 1
-        if not touching():
-            move_tail()
-            tail_visits.add((tx, ty))
+            knots[0][1] -= 1
+        for i in range(len(knots) - 2 + 1):
+            if not touching(knots[i][0], knots[i][1], knots[i+1][0], knots[i+1][1]):
+                new_pos = move_tail(knots[i][0], knots[i][1], knots[i+1][0], knots[i+1][1])
+                knots[i+1] = new_pos
+        tail_visits.add(tuple(knots[1]))
+        tail_visits_2.add(tuple(knots[-1]))
+
 
 print(f"Part 1: {len(tail_visits)}")
+print(f"Part 2: {len(tail_visits_2)}")
