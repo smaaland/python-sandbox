18e5d9e3afe0b6a59f8a2e86c6945e904d55759a
smaaland <andreas.karlsson@billogram.com>
2022-12-15 10:43:53 +0100

    day 10 part 2

--- adventofcode/2022/day10.py
+++ adventofcode/2022/day10.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 lines = [line.strip() for line in open("input10.txt", "r").readlines()]
 x = 1
+pixels = []
 ptr = 0
 sig_str = 0
 
@@ -23,6 +24,13 @@ for i in range(max(executions)+1):
             x += int(parts[1])
     if (i + 1) == 20 or (i + 1 + 20) % 40 == 0:
         sig_str += (i+1) * x
-
+    if abs(i % 40 - x) <= 1:
+        pixels.append("#")
+    else:
+        pixels.append(".")
 
 print(f"Part 1: {sig_str}")
+print("Part 2:")
+rows = [pixels[i:i + 40] for i in range(0, len(pixels), 40)]
+for r in rows:
+    print(''.join(r))
