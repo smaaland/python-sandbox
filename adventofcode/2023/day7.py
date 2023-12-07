aeedfdb72e946e648be1263c2baf08fb3fdb7131
smaaland <andreas.karlsson@billogram.com>
2023-12-07 10:31:24 +0100

    day 7 part 2

--- adventofcode/2023/day7.py
+++ adventofcode/2023/day7.py
@@ -4,10 +4,7 @@ from collections import Counter
 lines = [line.strip() for line in open("input7.txt", "r").readlines()]
 
 
-def compare_hands(x, y):
-    card_order = "AKQJT98765432"
-    cx = Counter(x[0])
-    cy = Counter(y[0])
+def compare_hands(cx, cy, x, y, card_order):
     if max(cx.values()) > max(cy.values()):
         return 1
     elif max(cx.values()) < max(cy.values()):
@@ -26,7 +23,37 @@ def compare_hands(x, y):
     return 0
 
 
+def compare_hands_1(x, y):
+    card_order = "AKQJT98765432"
+    cx = Counter(x[0])
+    cy = Counter(y[0])
+    return compare_hands(cx, cy, x, y, card_order)
+
+
+def compare_hands_2(x, y):
+    card_order = "AKQT98765432J"
+    cx = Counter(x[0])
+    cy = Counter(y[0])
+
+    for c in cx, cy:
+        if "J" in c:
+            jokers = c["J"]
+            del c["J"]
+            most_common = c.most_common(1)
+            if most_common:
+                c[most_common[0][0]] += jokers
+            else:
+                c["J"] = jokers
+
+    return compare_hands(cx, cy, x, y, card_order)
+
+
 hands = [(x.split(" ")[0], int(x.split(" ")[1])) for x in lines]
-sorted_hands = sorted(hands, key=functools.cmp_to_key(compare_hands))
-scores = [(i + 1) * hand[1] for i, hand in enumerate(sorted_hands)]
-print(f"Part 1: {sum(scores)}")
+
+sorted_hands_1 = sorted(hands, key=functools.cmp_to_key(compare_hands_1))
+scores_1 = [(i + 1) * hand[1] for i, hand in enumerate(sorted_hands_1)]
+print(f"Part 1: {sum(scores_1)}")
+
+sorted_hands_2 = sorted(hands, key=functools.cmp_to_key(compare_hands_2))
+scores_2 = [(i + 1) * hand[1] for i, hand in enumerate(sorted_hands_2)]
+print(f"Part 2: {sum(scores_2)}")
