05f5ba2c1f2bf44c40ce56a2df8aa0bbe429d643
smaaland <andreas.karlsson@billogram.com>
2024-12-05 22:38:46 +0100

    day 2

---
+++ adventofcode/2024/day2.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+lines = [[int(x) for x in line.strip().split()] for line in open("input2.txt", "r").readlines()]
+
+safe = 0
+safe_with_dampener = 0
+
+def is_safe(l: List) -> bool:
+    return (all(x > 0 for x in l) or all(x < 0 for x in l)) and all(1 <= abs(x) <= 3 for x in l)
+
+def get_diffs(l: List) -> List[int]:
+    return [a-b for a, b in zip(l, l[1:])]
+
+for line in lines:
+    diffs = get_diffs(line)
+    if is_safe(diffs):
+        safe += 1
+        safe_with_dampener += 1
+    else:
+        for i in range(len(line)):
+            if is_safe(get_diffs(line[:i] + line[i+1:])):
+                safe_with_dampener += 1
+                break
+
+print(f"Part 1: {safe}")
+print(f"Part 2: {safe_with_dampener}")
